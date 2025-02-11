39346121d1754d11556b2cb2b88340aba80e4be9
Fedro Togatorop <fedrotoga@gmail.com>
2025-02-11 23:59:31 +0700

    menambahkan src dan if statement

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log="Hallo nama saya adalah Fedro Saut Wibsiono Togatorop";
+// console.log="Hallo nama saya adalah Fedro Saut Wibsiono Togatorop";
+if (1+1===2) {
+    console.log("Betuk");
+}
