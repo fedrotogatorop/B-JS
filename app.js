6612c2ca134dfa46bdaf1e0d69f0d6e0d4531ef9
Fedro Togatorop <fedrotoga@gmail.com>
2025-02-12 13:10:46 +0700

    menambahkan if else math

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // console.log="Hallo nama saya adalah Fedro Saut Wibsiono Togatorop";
 
-console.log("before differ");
+// console.log("before differ");
 
-if (1+1===3  ) {
-    console.log("true");
+// if (1+1===3  ) {
+//     console.log("true");
+// }else{
+//     console.log("wrong");
+// }
+
+let angka = Math.random();
+console.log(angka);
+if (angka>=0.5) {
+    console.log("The number is bigger than");
 }else{
-    console.log("wrong");
+    console.log("The number is smaller than");
 }
