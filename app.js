e9ae394f8353e3c543babc05b77852d094757715
Fedro Togatorop <fedrotoga@gmail.com>
2025-02-23 20:00:30 +0700

    added looping for

--- app.js
+++ app.js
@@ -104,6 +104,18 @@
 // console.log((playlist[2] = "Hijau"));
 
 // Pengulangan
-for (let i = 1; i < 10; i += 1) {
+// for (let i = 1; i < 10; i += 1) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 20; i += 2) {
+//   console.log(i);
+// }
+
+for (let i = 100; i >= 0; i -= 1) {
+  console.log(i);
+}
+
+for (let i = 0; i <= 10; i += 2) {
   console.log(i);
 }
